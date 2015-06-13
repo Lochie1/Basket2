8d1fa7858546241506ff802a7d8d6eb5c89c6230
Lochie <Lochsta.E@hotmail.com>
2015-06-13 22:28:30 +0800

    Initial commit

---
+++ app/src/main/java/com/basket/tmail/basket2/MainMenu.java
@@ -0,0 +1,59 @@
+package com.basket.tmail.basket2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        RelativeLayout basketlayout = new RelativeLayout(this);
+
+        Button messagebutton = new Button(this);
+
+        basketlayout.addView(messagebutton);
+        setContentView(basketlayout);
+
+
+        setContentView(R.layout.activity_menu); }
+
+        public void messages(View view){
+           Intent intent = new Intent(MainMenu.this, messages.class);
+            startActivity(intent);}
+
+        public void email(View view){
+           Intent intent1 = new Intent(MainMenu.this, email.class);
+            startActivity(intent1);}
+
+        public void settings(View view){
+           Intent intent2 = new Intent(MainMenu.this, Settings.class);
+           startActivity(intent2);}
+
+
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/basket/tmail/basket2/mainscreen.java
@@ -0,0 +1,43 @@
+package com.basket.tmail.basket2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class mainscreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(mainscreen.this, MainMenu.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+                overridePendingTransition(R.anim.mainfadein, R.anim.splashfadeout);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
